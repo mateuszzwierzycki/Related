6cbfa512f29200c8ebd7af409fcba7bf3ba328c0
Mateusz Zwierzycki <mateuszzwierzycki@live.com>
2017-04-23 03:21:11 +0200

    more code

--- Relations.Core/Edges/Edges.cs
+++ Relations.Core/Edges/Edges.cs
@@ -5,8 +5,134 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Relations.Edges {
-    class Edges {
 
+    [Serializable]
+    class UndirectedEdge : EdgeBase, IComparable<UndirectedEdge> {
+
+        public UndirectedEdge() : base() { }
+        public UndirectedEdge(int A, int B) : base(A, B) { }
+        public UndirectedEdge(EdgeBase Other) : base(Other) { }
+
+        public int CompareTo(UndirectedEdge Other) {
+            if (this.Minimum() < Other.Minimum()) { return -1; }
+            if (this.Minimum() > Other.Minimum()) { return 1; }
+            if (this.Maximum() < Other.Maximum()) { return -1; }
+            if (this.Maximum() > Other.Maximum()) { return 1; }
+            return 0;
+        }
+
+        public override EdgeBase Duplicate() { return new UndirectedEdge(this); }
+
+        public void Orient() {
+            if (this.PointA > this.PointB) { this.Flip(); }
+        }
+
+        public override string ToString() {
+            return "Undirected" + base.ToString();
+        }
 
     }
+
+    [Serializable]
+    public class UndirectedEdge<T> : EdgeBase, IComparable<UndirectedEdge<T>> where T : IComparable<T> {
+
+        T myval = default(T);
+
+        public T Value {
+            get { return myval; }
+            set { myval = value; }
+        }
+
+        public UndirectedEdge(int A, int B, T Value) : base(A, B) { this.Value = Value; }
+
+        public int CompareTo(UndirectedEdge<T> Other) {
+            if (this.Minimum() < Other.Minimum()) { return -1; }
+            if (this.Minimum() > Other.Minimum()) { return 1; }
+            if (this.Maximum() < Other.Maximum()) { return -1; }
+            if (this.Maximum() > Other.Maximum()) { return 1; }
+            return this.Value.CompareTo(Other.Value);
+        }
+
+        public override EdgeBase Duplicate() { return new UndirectedEdge<T>(this.PointA, this.PointB, this.Value); }
+
+        public void Orient() {
+            if (this.PointA > this.PointB) { this.Flip(); }
+        }
+
+        public override string ToString() {
+            return "UndirectedEdge (" + PointA.ToString() + "-" + PointB.ToString() + "," + this.Value.ToString() + ")";
+        }
+
+    }
+
+    [Serializable]
+    class DirectedEdge : EdgeBase, IComparable<DirectedEdge> {
+
+        public DirectedEdge() : base() { }
+        public DirectedEdge(int From, int To) : base(From, To) { }
+        public DirectedEdge(EdgeBase Other) : base(Other) { }
+
+        public int From { get => PointA; set => PointA = value; }
+        public int To { get => PointB; set => PointB = value; }
+
+        public int CompareTo(DirectedEdge Other) {
+            if (this.From < Other.From) { return -1; }
+            if (this.From > Other.From) { return 1; }
+            if (this.To < Other.To) { return -1; }
+            if (this.To > Other.To) { return 1; }
+            return 0;
+        }
+
+        public override EdgeBase Duplicate() { return new DirectedEdge(this); }
+
+        public void Orient() {
+            if (this.PointA > this.PointB) { this.Flip(); }
+        }
+
+        public override string ToString() {
+            return "Directed" + base.ToString();
+        }
+
+    }
+
+    [Serializable]
+    public class DirectedEdge<T> : EdgeBase, IComparable<DirectedEdge<T>> where T : IComparable<T> {
+
+        T myval = default(T);
+
+        public T Value {
+            get { return myval; }
+            set { myval = value; }
+        }
+
+        public int From { get => PointA; set => PointA = value; }
+        public int To { get => PointB; set => PointB = value; }
+
+        public DirectedEdge(int From, int To, T Value) : base(From, To) { this.Value = Value; }
+
+        public int CompareTo(DirectedEdge<T> Other) {
+            if (this.Minimum() < Other.Minimum()) { return -1; }
+            if (this.Minimum() > Other.Minimum()) { return 1; }
+            if (this.Maximum() < Other.Maximum()) { return -1; }
+            if (this.Maximum() > Other.Maximum()) { return 1; }
+            return this.Value.CompareTo(Other.Value);
+        }
+
+        public override EdgeBase Duplicate() { return new DirectedEdge<T>(this.PointA, this.PointB, this.Value); }
+
+        public void Orient() {
+            if (this.PointA > this.PointB) { this.Flip(); }
+        }
+
+        public override string ToString() {
+            return "DirectedEdge (" + PointA.ToString() + "-" + PointB.ToString() + "," + this.Value.ToString() + ")";
+        }
+
+    }
+
+
+
+
 }
+
+
---
+++ Relations.Core/Edges/GraphEdgeList.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Relations.Graphs;
+
+namespace Relations.Edges {
+    class GraphEdgeList<T> : ICollection<T> where T : EdgeBase {
+
+        private SortedList<T, T> _data = new SortedList<T, T>;
+        private Graphs.GraphBase _owner = null;
+        private SortedList<T, T> Data { get => _data; set => _data = value; }
+        private GraphBase Owner { get => _owner; set => _owner = value; }
+
+        public bool IsReadOnly => false;
+
+        int ICollection<T>.Count => Data.Count();
+
+        internal GraphEdgeList(GraphBase Owner) : base() { this.Owner = Owner; }
+
+        public T this[T Key] {
+            get { return Data[Key]; }
+            set { Data[Key] = value; }
+        }
+
+        public void Add(T item) {
+            if (item.PointA > Owner.VertexCount - 1) { throw new IndexOutOfRangeException(); }
+            if (item.PointB > Owner.VertexCount - 1) { throw new IndexOutOfRangeException(); }
+        }
+
+        public void AddRange(IEnumerable<T> Values) {
+            foreach (T item in Values) {
+                this.Add(item);
+            }
+        }
+
+        public bool Remove(T item) {
+            Data.Remove(item);
+            return true;
+        }
+
+        public void RemoveAt(int index) { Data.RemoveAt(index); }
+
+        public void Clear() { Data.Clear(); }
+
+        public void CleanUp() {
+            SortedList<T, T> nl = new SortedList<T, T>();
+
+            foreach (T ed in this) {
+                if (ed.IsValid()) { nl[ed] = [ed];  }
+            }
+
+            this._data = nl;
+        }
+
+        public void OnVertexRemove(int Vertex) {
+
+            foreach (EdgeBase ed in this) {
+                EdgeBase thised = ed;
+                this.Remove((T)thised);  //TODO check if this works, casting seems unnecessary.
+
+                if (thised.PointA > Vertex) { thised.PointA -= 1; } 
+                else if(thised.PointA == Vertex) { thised.PointA = -1; }
+
+                if (thised.PointB > Vertex) { thised.PointB -= 1; }
+                else if (thised.PointB == Vertex) { thised.PointB = -1; }
+
+                this.= thised;
+            }
+
+        }
+
+        public bool Contains(T item) {
+            return Data.ContainsValue(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex) {
+            for (int i = 0; i < this.Count(); i++) {
+                array[arrayIndex + i] = Data.Keys[i];
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator() {
+            return Data.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return Data.Values.GetEnumerator();
+        }
+    }
+}
