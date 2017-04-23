3bff2ab24d15aa6c62a14059c4127a2f6ba9b47d
Mateusz Zwierzycki <mateuszzwierzycki@live.com>
2017-04-23 16:22:10 +0200

    sets

---
+++ Related.Core/Sets/SetsUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Related.Sets {
+    public class SetsUtils {
+
+        static double JaccardCoefficient (HashSet<object>A , HashSet<object> B) {
+            HashSet<object> Sum = new HashSet<object>(A);
+
+            foreach (object item in B ) {
+                Sum.Add(item); 
+            }
+
+            return (double)A.Count / (double)(Sum.Count - A.Count); 
+        }
+    }
+}
